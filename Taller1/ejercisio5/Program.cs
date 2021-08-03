2bf22986c73a0958e702284957ece84ce9d26d41
Alex <Suarezalex596@gmail.com>
2021-08-02 20:14:24 -0500

    Taller1 -> ejercisio5 terminado

---
+++ Taller1/ejercisio5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ejercisio5
+{
+    class Program
+    {
+        private double distancia;
+        private int tiempo;
+        static void Main(string[] args)
+        {
+            new Program();
+        }
+
+        public Program()
+        {
+            Console.Write("Ingrese la distancia [km]: ");
+
+            distancia = double.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el tiempo [horas]: ");
+
+            tiempo = int.Parse(Console.ReadLine());
+
+            calcVelocidad();
+        }
+
+        public void calcVelocidad()
+        {
+            Console.WriteLine("La velocidad de desplazamiento es: " + distancia / tiempo + "km/h");
+        }
+    }
+}
