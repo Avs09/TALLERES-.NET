fbfd51248625d60d761465f9b9b599ee1c986a13
Alex <Suarezalex596@gmail.com>
2021-08-02 23:52:42 -0500

    Taller1 -> ejercisio12 terminado

---
+++ Taller1/ejercisio12/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ejercisio12
+{
+    class Program
+    {
+        private int cantidadM, cantidadF;
+        static void Main(string[] args)
+        {
+            new Program();
+        }
+
+        public Program()
+        {
+            Console.Write("Ingrese la cantidad de estudiantes masculinos: ");
+
+            cantidadM = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese la cantidad de estudiantes femeninos: ");
+
+            cantidadF = int.Parse(Console.ReadLine());
+
+            estudiantesPorc();
+        }
+
+        public void estudiantesPorc()
+        {
+            double totalEstudiantes = cantidadF + cantidadM;
+
+            Console.WriteLine("Porcentaje de estudiantes femeninos: " + Math.Round((cantidadF / totalEstudiantes) * 100) + "%");
+
+            Console.WriteLine("Porcentaje de estudiantes masculinos: " + Math.Round((cantidadM / totalEstudiantes) * 100) + "%");
+
+            Console.WriteLine("Cantidad de estudiantes: " + totalEstudiantes);
+        }
+    }
+}
