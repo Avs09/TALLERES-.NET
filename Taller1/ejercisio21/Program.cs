59b72799c161fb0fa5e0d7407ce3b9474fbd316c
Alex <Suarezalex596@gmail.com>
2021-08-03 12:53:26 -0500

    Taller1 -> ejercisio21 terminado

---
+++ Taller1/ejercisio21/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ejercisio21
+{
+    class Program
+    {
+        private String aprendiz, direccion;
+        private int año;
+        
+        static void Main(string[] args)
+        {
+            new Program();
+        }
+
+        public Program()
+        {
+            Console.Write("Nombre del aprendiz: ");
+
+            aprendiz = Console.ReadLine();
+
+            Console.Write("Ingrese el año de nacimiento: ");
+
+            año = int.Parse(Console.ReadLine());
+
+            Console.Write("Direccion: ");
+
+            direccion = Console.ReadLine();
+
+            Console.WriteLine("Edad: " + calcEdad());
+        }
+
+        public int calcEdad()
+        {
+            DateTime fechaActual = DateTime.Now;
+
+            int añoActual = fechaActual.Year;
+
+            return añoActual - año;
+        }
+    }
+}
