fb5fa746c2a93eaf7b13938d75c03d7a86a889ac
Alex <Suarezalex596@gmail.com>
2021-08-02 19:55:42 -0500

    Taller1 -> ejercisio3 terminado

---
+++ Taller1/ejercisio3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ejercisio3
+{
+    class Program
+    {
+        public static double nota1, nota2, nota3;
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese la primera nota: ");
+
+            nota1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese la segunda nota: ");
+
+            nota2 = double.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese la tercera nota: ");
+
+            nota3 = double.Parse(Console.ReadLine());
+
+            definitivaN();
+        }
+
+        public static void definitivaN()
+        {
+            Console.WriteLine("La definitiva de las notas es: " +  (nota1 + nota2 + nota3) / 3);
+        }
+    }
+}
