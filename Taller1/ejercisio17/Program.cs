91b0b8281df202c0742c2c86fd587d184e16b732
Alex <Suarezalex596@gmail.com>
2021-08-03 11:25:32 -0500

    Taller1 -> ejercisio17 terminado

---
+++ Taller1/ejercisio17/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ejercisio17
+{
+    class Program
+    {
+        private double salario, ahorroMensual;
+
+        private String empleado;
+
+        static void Main(string[] args)
+        {
+            Program empleado1 = new Program();
+
+            empleado1.colillaPago();
+        }
+
+        public Program()
+        {
+            Console.Write("Nombre del empleado: ");
+
+            empleado = Console.ReadLine();
+
+            Console.Write("Ingrese el salario: ");
+
+            salario = double.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el ahorro mensual: ");
+
+            ahorroMensual = double.Parse(Console.ReadLine());
+        }
+
+        public void colillaPago()
+        {
+            double salud = salario * 0.125;
+
+            double pension = salario * 0.16;
+
+            Console.WriteLine("*** COLILLA DE PAGO ***");
+
+            Console.WriteLine("Empleado: " + empleado);
+
+            Console.WriteLine("Salario: $" + salario);
+
+            Console.WriteLine("Valor del ahorro mensual: $" + ahorroMensual);
+
+            Console.WriteLine("Valor por parte de la salud: $" + salud);
+
+            Console.WriteLine("Valor por parte de la pension: $" + pension);
+
+            Console.WriteLine("Total a recibir: $" + (salario - salud - pension));
+        }
+    }
+}
