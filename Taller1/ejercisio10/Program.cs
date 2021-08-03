be01f640d36083fb5eec7b77966a43c540fb3656
Alex <Suarezalex596@gmail.com>
2021-08-02 23:33:51 -0500

    Taller1 -> ejercisio10 terminado

---
+++ Taller1/ejercisio10/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ejercisio10
+{
+    class Program
+    {
+        private static double compra;
+        static void Main(String[] args)
+        {
+            Console.Write("Ingrese el valor de la compra: ");
+
+            compra = double.Parse(Console.ReadLine());
+
+            totalCompra();
+        }
+
+        public static void totalCompra()
+        {
+            double descuento = compra * 0.15;
+
+            Console.WriteLine("El total de la compra es: " + (compra - descuento));
+        }
+    }
+}
