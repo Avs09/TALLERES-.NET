bb6770fd0959ac248f0d5072a6a29163123d333f
Alex <Suarezalex596@gmail.com>
2021-08-03 00:02:11 -0500

    Taller1 -> ejercisio13 terminado

---
+++ Taller1/ejercisio13/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ejercisio13
+{
+    class Program
+    {
+        private static double valorH;
+        private static int horas;
+        static void Main(string[] args)
+        {
+            String trabajador;
+
+            Console.Write("Trabajador: ");
+
+            trabajador = Console.ReadLine();
+
+            Console.Write("Ingrese las horas: ");
+
+            horas = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el valor por hora: ");
+
+            valorH = double.Parse(Console.ReadLine());
+
+            calcSalario(trabajador);
+        }
+
+        public static void calcSalario(String t)
+        {
+            Console.WriteLine($"{t},tu salario es de: {valorH*horas}$");
+        }
+    }
+}
