3eec72ac6df38d5543ba2ba371d877e8e6ce8ef0
Alex <Suarezalex596@gmail.com>
2021-08-03 13:34:24 -0500

    Taller1 -> ejercisio23 terminado

---
+++ Taller1/ejercisio23/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ejercisio23
+{
+    class Program
+    {
+        private byte distancia;
+
+        static void Main(string[] args)
+        {
+            new Program();
+        }
+
+        public Program()
+        {
+            Console.Write("Ingrese la distancia (metros): ");
+
+            distancia = byte.Parse(Console.ReadLine());
+
+            Console.WriteLine(calcTiempoSubida());
+        }
+
+        public String calcTiempoSubida()
+        {
+            byte altura = 7;
+
+            if(distancia <=7)
+            {
+                return "Tiempo de subida: " + (distancia * 5) / altura + " horas";
+            }
+            else return "Error, La altura maxima es de 7m";
+        }
+    }
+}
