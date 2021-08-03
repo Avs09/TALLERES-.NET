fedb4937e206d015a1749999d78fa3fc2b7c22b2
Alex <Suarezalex596@gmail.com>
2021-08-03 01:03:49 -0500

    Taller1 -> ejercisio14 terminado

--- Taller1/ejercisio14/Program.cs
+++ Taller1/ejercisio14/Program.cs
@@ -4,9 +4,64 @@ namespace ejercisio14
 {
     class Program
     {
+        private String[] articulos;
+        private int[] unidades;
+        private double[] valor;
+        private double total, iva;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            new Program();
+        }
+
+        public Program()
+        {
+            articulos = new string[4];
+
+            valor = new double[4];
+
+            unidades = new int[4];
+
+            for(int i = 0; i < articulos.GetLength(0); i++)
+            {
+                Console.Write("\nIngrese el articulo N°" + (i+1) + ": ");
+
+                articulos[i] = Console.ReadLine();
+
+                Console.Write("Ingrese el valor del articulo: ");
+
+                valor[i] = double.Parse(Console.ReadLine());
+
+                Console.Write("¿Cuantas unidades quiere llevar?: ");
+
+                unidades[i] = int.Parse(Console.ReadLine());
+            }
+
+            getFactura();
+        }
+        public void getFactura()
+        {
+            Console.WriteLine("\n*** FACTURA ***");
+
+            for(int i = 0; i < articulos.GetLength(0); i++)
+            {
+                Console.WriteLine("Articulo: " + articulos[i]);
+
+                Console.WriteLine("Unidades: " + unidades[i]);
+
+                Console.WriteLine("Precio del articulo: $" + valor[i]);
+
+                Console.WriteLine("Valor del iva del articulo: $" + (valor[i] * unidades[i]) * 0.19 + "\n");
+
+                iva += (valor[i] * unidades[i]) * 0.19;
+
+                total+= (valor[i] * unidades[i]);
+
+                if(i == 3)
+                {
+                    Console.WriteLine("Total iva: $" + iva);
+                    Console.WriteLine($"Total a pagar: ${total+iva}"); 
+                }
+            }
         }
     }
 }
