db9008e4f85ab27c3db79f0a51500feba5a95d41
Alex <Suarezalex596@gmail.com>
2021-08-03 01:52:50 -0500

    Taller1 -> ejercisio16 terminado

---
+++ Taller1/ejercisio16/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ejercisio16
+{
+    class Program
+    {
+        private double valor, recaudo;
+        private int pasajero;
+        static void Main(string[] args)
+        {
+            new Program();
+        }
+
+        public Program()
+        {
+            calcGanancia();
+
+            Console.WriteLine("\nCantidad de pasajeros: " + pasajero);
+
+            Console.WriteLine("Total recaudo: $" + recaudo);
+
+            Console.WriteLine("Valor por pasajero: $" + valor);
+
+            Console.WriteLine("Ganancia: " + recaudo * 0.19);
+        }
+
+        public void calcGanancia()
+        {
+            Console.Write("Ingrese la cantidad de pasajeros: ");
+
+            pasajero = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el precio de subida: ");
+
+            valor = double.Parse(Console.ReadLine());
+
+            recaudo = pasajero * valor;
+        }
+    }
+}
