48d6f9ab8e8e3f7a64c937bd136ec6a8a9fe3e0c
Alex <Suarezalex596@gmail.com>
2021-08-02 19:34:05 -0500

    Taller1 -> Ejercisio 1 terminado

--- Taller1/ejercisio1/Program.cs
+++ Taller1/ejercisio1/Program.cs
@@ -6,7 +6,26 @@ namespace ejercisio1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double numero1, numero2, numero3;
+
+            Console.Write("Ingrese el primer numero: ");
+
+            numero1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el segundo numero: ");
+
+            numero2 = double.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el tercer numero: ");
+
+            numero3 = double.Parse(Console.ReadLine());
+            
+            suma(numero1, numero2, numero3);
+        }
+
+        public static void suma(double n1, double n2, double n3)
+        {
+            Console.WriteLine("La suma es: " + (n1 + n2 + n3));
         }
     }
 }
