e826ed59aa9c5aa33231b04de02ccc0a6f06ffff
Alex <Suarezalex596@gmail.com>
2021-08-04 14:00:17 -0500

    Taller2 -> parte2 terminado

---
+++ Taller2/parte1/ejercisio15/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ejercisio15
+{
+    class Program
+    {
+        private static double numero;
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un numero: ");
+
+            numero = double.Parse(Console.ReadLine());
+
+            cubo();
+        }
+
+        public static void cubo()
+        {
+            if(numero >= 10)
+            {
+                Console.WriteLine($"El cubo de {numero} es: {Math.Pow(numero, 3)}");
+            }
+        }
+    }
+}
